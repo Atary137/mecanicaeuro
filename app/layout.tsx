2247fc9b37c32dcdec5838d3fd4b7f0dc086b67c
Augustavicius <127162940+Atary137@users.noreply.github.com>
2026-03-25 11:07:23 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -111,6 +111,42 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body className={`${inter.variable} font-sans antialiased`}>
         {children}
+
+        {/* SEO LOCAL */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "AutoRepair",
+              "name": "Mecânica Euro",
+              "image": "https://mecanicaeuro.com.br/images/logo-euro.png",
+              "url": "https://mecanicaeuro.com.br",
+              "telephone": "+5541999491129",
+              "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "Avenida Rui Barbosa, 10703", 
+                "addressLocality": "São José dos Pinhais",
+                "addressRegion": "PR",
+                "postalCode": "83025-010",
+                "addressCountry": "BR"
+              },
+              "geo": {
+                "@type": "GeoCoordinates",
+                "latitude": -25.55095513874384,
+                "longitude": -49.20828015481486
+              },
+              "openingHoursSpecification": [
+                {
+                  "@type": "OpeningHoursSpecification",
+                  "dayOfWeek": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+                  "opens": "08:00",
+                  "closes": "18:00"
+                }
+              ]
+            })
+          }}
+        />
       </body>
     </html>
   )
